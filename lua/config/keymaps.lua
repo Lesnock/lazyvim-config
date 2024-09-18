a7f5375217dbe369120f6be637e070c0d3011cb0
Caio Lesnock <caio.lesnock@metadil.com.br>
2024-09-18 10:01:19 -0300

    Config dap keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-J>", function()
+vim.keymap.set("n", "<leader>th", function()
   vim.cmd([[ToggleTerm]])
 end, { desc = "Open terminal" })
+
+-- Debugging
+-- stylua: ignore start
+vim.keymap.set({ "n", "v" }, "<F5>", function() require("dap").continue() end, { desc = "Continue" })
+vim.keymap.set({ "n", "v" }, "<F9>", function() require("dap").toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
+vim.keymap.set({ "n", "v" }, "<F10>", function() require("dap").step_over() end, { desc = "Step Over" })
+vim.keymap.set({ "n", "v" }, "<F11>", function() require("dap").step_into() end, { desc = "Step Into" })
+-- stylua: ignore end
