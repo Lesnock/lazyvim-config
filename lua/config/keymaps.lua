dca42854a8d4baf5d29f0c50126394cb6021cc30
Caio Lesnock <caio.lesnock@metadil.com.br>
2024-09-20 16:40:04 -0300

    Add keymap to centralize screen

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,10 +9,11 @@ vim.keymap.set("n", "<leader>th", function()
   vim.cmd([[ToggleTerm]])
 end, { desc = "Open terminal" })
 
--- Debugging
 -- stylua: ignore start
 vim.keymap.set({ "n", "v" }, "<F5>", function() require("dap").continue() end, { desc = "Continue" })
 vim.keymap.set({ "n", "v" }, "<F9>", function() require("dap").toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
 vim.keymap.set({ "n", "v" }, "<F10>", function() require("dap").step_over() end, { desc = "Step Over" })
 vim.keymap.set({ "n", "v" }, "<F11>", function() require("dap").step_into() end, { desc = "Step Into" })
 -- stylua: ignore end
+
+vim.keymap.set({ "n" }, "<cr>", "zz", { desc = "Centralize screen" })
