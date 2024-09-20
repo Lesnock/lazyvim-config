d2222494611b2887bcd3e8fbda14ef1c8d8c5f9b
Caio Lesnock <caio.lesnock@metadil.com.br>
2024-09-20 13:36:25 -0300

    Copy to clipboard

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,14 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
   pattern = "*.http",
   command = "set filetype=http",
 })
+
+-- Copy to clipboard
+vim.opt.clipboard = "unnamedplus"
+if vim.fn.has("wsl") == 1 then
+  vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+    callback = function()
+      vim.fn.system("clip.exe", vim.fn.getreg('"'))
+    end,
+  })
+end
