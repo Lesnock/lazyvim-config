228d40cbaba55489fbdcab132b705d420cf94db8
Caio Lesnock <caio.dev@outlook.com>
2025-07-31 11:22:39 -0300

    Add session autoload

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,17 +3,26 @@
 -- Add any additional autocmds here
 
 vim.api.nvim_create_autocmd("BufWinEnter", {
-  pattern = "*.http",
-  command = "set filetype=http",
+    pattern = "*.http",
+    command = "set filetype=http",
 })
 
 -- Copy to clipboard
 vim.opt.clipboard = "unnamedplus"
 if vim.fn.has("wsl") == 1 then
-  vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("Yank", { clear = true }),
-    callback = function()
-      vim.fn.system("clip.exe", vim.fn.getreg('"'))
-    end,
-  })
+    vim.api.nvim_create_autocmd("TextYankPost", {
+        group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+        callback = function()
+            vim.fn.system("clip.exe", vim.fn.getreg('"'))
+        end,
+    })
 end
+
+-- Automatically load last session of direction
+vim.api.nvim_create_autocmd("VimEnter", {
+    group = vim.api.nvim_create_augroup("restore_session", { clear = true }),
+    callback = function()
+        require("persistence").load()
+    end,
+    nested = true,
+})
